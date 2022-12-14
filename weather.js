e9266484db13d40f62c5a1a5529a47863c83c98d
Hadi Prassetio <hadiprassetio516@gmail.com>
2022-12-14 07:30:43 +0700

    Add Weather.js

---
+++ weather.js
@@ -0,0 +1,19 @@
+import fetch from "node-fetch"
+
+var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+const getData =async()=>{
+  const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?lat=-6.2146&lon=106.8451&appid=5d9e2c77930406aedfd99b505c7b2012')
+  let data = await response.json()
+  let result = data.list
+  result.map((item,index)=>{
+    if(index % 8 == 0){
+      let date = new Date(item.dt_txt)
+      const newDate = date.toLocaleDateString("en-US",options)
+      console.log(newDate,`: ${Math.floor(item.main.temp-273.15)}℃`)
+    }
+  })
+}
+getData()
+
+// fetch('https://api.openweathermap.org/data/2.5/forecast?lat=-6.2146&lon=106.8451&appid=5d9e2c77930406aedfd99b505c7b2012')
+// .then(res =>console.log(res))
